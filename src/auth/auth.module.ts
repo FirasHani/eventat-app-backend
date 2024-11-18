2578fc735cee198769ed73a76354af4bc41ce8a4
FirasHani <77682961+FirasHani@users.noreply.github.com>
2024-11-18 17:27:15 +0300

    make forwardRef

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef  } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import{ UserModule } from '../user/user.module'
+import{ UserModule } from '../user/user.module';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  imports:[UserModule ],
+  imports: [forwardRef(() => UserModule), DatabaseModule],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module,forwardRef } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { DatabaseModule } from 'src/database/database.module';
+import { AuthModule } from '../auth/auth.module';
+
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, forwardRef(() => AuthModule)],
   controllers: [UserController],
   providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
