86d2efd66ad744eeec597241ecfee832e39416dc
FirasHani <77682961+FirasHani@users.noreply.github.com>
2024-11-18 17:50:33 +0300

    create JWT and auth guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,17 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import{ UserModule } from '../user/user.module';
 import { DatabaseModule } from 'src/database/database.module';
+import { jwtConstants } from './constants';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [forwardRef(() => UserModule), DatabaseModule],
+  imports: [forwardRef(() => UserModule), DatabaseModule, 
+    JwtModule.register({
+    global: true,
+    secret: jwtConstants.secret,
+    signOptions: { expiresIn: '60s' },
+   }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [AuthService],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,22 @@
-
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UserService) {}
+  constructor(private usersService: UserService,private jwtService: JwtService) {}
 
-  async signIn(username: string, pass: string): Promise<any> {
+  async signIn(
+    username: string,
+    pass: string,
+  ): Promise<{ access_token: string }> {
     const user = await this.usersService.findOne(username);
     if (user?.password !== pass) {
       throw new UnauthorizedException();
     }
-    const { password, ...result } = user;
-    // TODO: Generate a JWT and return it here
-    // instead of the user object
-    return result;
+    const payload = { sub: user.id, username: user.name };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
