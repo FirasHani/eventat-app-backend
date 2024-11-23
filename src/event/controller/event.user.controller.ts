d87c7c6abb11cc19767ae2b73d8419cd08d15833
FirasHani <77682961+FirasHani@users.noreply.github.com>
2024-11-23 21:24:20 +0300

    join to event

--- src/event/controller/event.user.controller.ts
+++ src/event/controller/event.user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Request, Injectable  } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete,Put, Request, Injectable, Req  } from '@nestjs/common';
 import { EventService } from '../service/event.service';
 import { EventUserService } from '../service/event.user.service';
 import { Prisma } from '@prisma/client';
@@ -8,9 +8,13 @@ import { Role } from 'src/role/role.enum';
 @Controller('event-user')
 export class EventUserController {
 
-    constructor (private readonly eventService:EventService, eventUserService : EventUserService){ }
+    constructor (private readonly  eventUserService : EventUserService){ }
     
-    
-    
+    @Put(':id')
+    @Roles(Role.User)
+    joinEvent(@Param('id') id:string, @Request() user){
+          return this.eventUserService.joinEvent(+id,user);
+    }
 
-}
+
+} 
--- src/event/service/event.user.service.ts
+++ src/event/service/event.user.service.ts
@@ -4,5 +4,29 @@ import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class EventUserService{
+    
+    constructor(private readonly databaseService: DatabaseService) { }
 
+    async joinEvent(eventId: number, userId: number) {
+        const existing = await this.databaseService.joined_Event.findFirst({
+          where: { 
+            eventId:23, 
+            userId : 28
+        },
+        });
+      
+        if (existing) {
+          return "User already joined this event.";
+        }
+      
+        await this.databaseService.joined_Event.create({
+          data: { 
+             eventId:23,
+             userId:28
+         },
+        });
+      
+        return "User successfully joined the event.";
+      }
+      
 }
