12731429c3c9b5d5e7e6a323acf9452026400dcd
FirasHani <77682961+FirasHani@users.noreply.github.com>
2024-11-25 20:14:51 +0300

    fixed the club model

--- prisma/migrations/20241125150520_add_user_member_model/migration.sql
+++ prisma/migrations/20241125150520_add_user_member_model/migration.sql
@@ -2,9 +2,19 @@
 CREATE TABLE "Club" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
-    "memberId" INTEGER[],
 
     CONSTRAINT "Club_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User_Member" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "clubId" INTEGER NOT NULL,
+    "joinedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "User_Member_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -14,13 +24,19 @@ CREATE TABLE "_ClubToEvent" (
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "User_Member_userId_clubId_key" ON "User_Member"("userId", "clubId");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "_ClubToEvent_AB_unique" ON "_ClubToEvent"("A", "B");
 
 -- CreateIndex
 CREATE INDEX "_ClubToEvent_B_index" ON "_ClubToEvent"("B");
 
 -- AddForeignKey
-ALTER TABLE "Club" ADD CONSTRAINT "Club_memberId_fkey" FOREIGN KEY ("memberId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "User_Member" ADD CONSTRAINT "User_Member_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "User_Member" ADD CONSTRAINT "User_Member_clubId_fkey" FOREIGN KEY ("clubId") REFERENCES "Club"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_ClubToEvent" ADD CONSTRAINT "_ClubToEvent_A_fkey" FOREIGN KEY ("A") REFERENCES "Club"("id") ON DELETE CASCADE ON UPDATE CASCADE;
