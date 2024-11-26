48643aede58fbbf3418e7c8f7375e3cb3c16f6a2
FirasHani <77682961+FirasHani@users.noreply.github.com>
2024-11-26 23:50:18 +0300

    add roles

--- src/club/controller/club.controller.ts
+++ src/club/controller/club.controller.ts
@@ -1,17 +1,20 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { ClubService } from '../service/club.service';
 import { Prisma } from '@prisma/client';
-
+import { Roles } from '../../role/role.decorator';
+import { Role } from 'src/role/role.enum';
 
 @Controller('club')
 export class ClubController {
   constructor(private readonly clubService: ClubService) {}
 
+  @Roles(Role.Google,Role.User,Role.Admin)
   @Post()
   create(@Body() createClubDto: Prisma.ClubCreateInput) {
     return this.clubService.create(createClubDto);
   }
 
+  @Roles(Role.Google,Role.User,Role.Admin)
   @Get()
   findAll() {
     return this.clubService.findAll();
