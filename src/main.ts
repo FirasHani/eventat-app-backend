5458780f44efcb302b0eb20760efe778077cb32b
FirasHani <77682961+FirasHani@users.noreply.github.com>
2024-11-30 22:09:50 +0300

    update CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  // Add CORS using Express middleware
+  app.use(cors({
+    origin: 'http://localhost:8081',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  }));
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
