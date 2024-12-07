279e32f125b8d1671c8873ab579cd5114ffcf434
FirasHani <77682961+FirasHani@users.noreply.github.com>
2024-12-07 17:05:49 +0300

    update vercel

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
+import { VercelRequest, VercelResponse } from '@vercel/node';
 
-async function bootstrap() {
+async function createNestServer() {
   const app = await NestFactory.create(AppModule);
-
 
   // Add CORS using Express middleware
   app.use(cors({
-    origin: 'http://localhost:8081',
+    origin: 'http://localhost:8081', // Adjust this to your frontend URL if necessary
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   }));
 
-
-  await app.listen(process.env.PORT ?? 3000);
+  return app;
 }
-bootstrap();
+
+// Start the NestJS application locally
+async function bootstrap() {
+  const app = await createNestServer();
+  await app.listen(process.env.PORT || 3000);
+  console.log(`Application is running on: http://localhost:${process.env.PORT || 3000}`);
+}
+
+// Export the handler for Vercel
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const app = await createNestServer();
+  await app.init(); // Initialize the app
+  return app.getHttpAdapter().getInstance().handle(req, res); // Handle requests
+}
+
+// If not in Vercel, start the application
+if (process.env.VERCEL === undefined) {
+  bootstrap();
+}
