38ed1b51d14d52b6e7f06ff3f3bbfa16254fb45c
FirasHani <77682961+FirasHani@users.noreply.github.com>
2024-12-25 23:26:56 +0300

    update the time

--- src/event/service/event.service.ts
+++ src/event/service/event.service.ts
@@ -1,29 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { DatabaseService } from 'src/database/database.service';
+import * as moment from 'moment'; 
 
 @Injectable()
  export class EventService {
 
   constructor(private readonly databaseService: DatabaseService) { }
 
-async createEvent(createEvent: Prisma.EventCreateInput,userId:number, creatorName:string) {
-
-  return await this.databaseService.event.create({
-    data: {
-      createrName:creatorName,
-      event_name: createEvent.event_name,
-      event_desc: createEvent.event_desc,
-      posters: createEvent.posters || [],
-      event_date: createEvent.event_date ? new Date(createEvent.event_date) : null,
-      faculty: createEvent.faculty || null,
-      floor: createEvent.floor || null,
-      room: createEvent.room || null,
-      image: createEvent.image || null, 
-      user: { connect: { id: userId } },
-    },
-  });
-}
+  async createEvent(
+    createEvent: Prisma.EventCreateInput,
+    userId: number,
+    creatorName: string,
+  ) {
+    const eventDate = createEvent.event_date
+      ? moment(createEvent.event_date, 'DD-MM-YYYY h:mm A').toDate()
+      : null;
+    
+    if (eventDate && isNaN(eventDate.getTime())) {
+      throw new Error('Invalid date provided');
+    }
+  
+    return await this.databaseService.event.create({
+      data: {
+        createrName: creatorName,
+        event_name: createEvent.event_name,
+        event_desc: createEvent.event_desc,
+        posters: createEvent.posters || [],
+        event_date: eventDate,
+        faculty: createEvent.faculty || null,
+        floor: createEvent.floor || null,
+        room: createEvent.room || null,
+        image: createEvent.image || null,
+        user: { connect: { id: userId } },
+      },
+    });
+  }
 
  async findAll() {
     const events = await this.databaseService.event.findMany({
