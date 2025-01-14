45603ee279db2eeede32c1f2aa7e3f94363a5374
FirasHani <77682961+FirasHani@users.noreply.github.com>
2025-01-14 23:22:04 +0300

    Update club.controller.ts

--- src/club/controller/club.controller.ts
+++ src/club/controller/club.controller.ts
@@ -8,13 +8,11 @@ import { Role } from '../../role/role.enum';
 export class ClubController {
   constructor(private readonly clubService: ClubService) {}
 
-  @Roles(Role.User,Role.Admin)
   @Post()
   create(@Body() createClubDto: Prisma.ClubCreateInput) {
     return this.clubService.create(createClubDto);
   }
 
-  @Roles(Role.User,Role.Admin)
   @Get()
   findAll() {
     return this.clubService.findAll();
